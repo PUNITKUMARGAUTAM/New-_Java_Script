9330d26417c3031f369431a231b6faf2f0ee865f
PUNIT KUMAR GAUTAM <82086530+PUNITKUMARGAUTAM@users.noreply.github.com>
2024-12-18 13:14:50 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+//console.log("Hello i am able to run js code");
+
+// var a = 50;
+
+// var a =90;
+
+// var a = 100;
+
+// console.log(a);
+
+// let b = 24;
+
+// console.log(typeof(b));
+
+// var student = {
+
+
+//     name : 'Abhimanyu',
+//     age : 24,
+//     isPass : true,
+// };
+
+// student["age"] = student["age"] + 1;
+
+//console.log(student["age"]);
+
+const profile = {
+
+    userName : "@PunitKumar",
+    folowers : 500,
+    post : 35,
+    sociamMidApp : "instagram",
+    isFollow : true,
+}
+
+console.log(typeof(profile["folowers"]));
---
+++ script.js
@@ -0,0 +1,96 @@
+//console.log("Hello world! df");
+
+let a = 2;
+
+let b = 7;
+
+//  let c = a+b;
+
+console.log("Add = ", a+b);
+console.log("Mul = ", a*b);
+console.log("Sub = ", a-b);
+console.log("Moodules = ", a%b);
+console.log("divide = ",a/b);
+console.log("a**b", a**b); 
+
+
+// console.log("A == " + a + "&" + "B ==" + b);
+
+// console.log("a++ = "  + a++);
+// console.log("a-- = " + a--);
+
+// console.log("++a= "  + ++a);
+// console.log("--a= " + --a);
+
+// console.log("A == B" + a===b);
+
+// Conditional statement
+
+// let age = 18;
+
+// if( age >= 18)
+// {
+//     console.log("You can vote");
+// }
+
+// else 
+// {
+//     console.log("You can not  vote");
+// }
+
+// let num = 15;
+
+// if(num % 2 === 0)
+// {
+//     console.log("Num is even " + num);
+
+// }
+
+// else{
+
+//     console.log("Num is odd " + num);
+// }
+
+
+// let mode = 'pink';
+// let color;
+
+// if( mode === 'dark')
+// {
+//     color = 'dark';
+// }
+
+// else if(mode === 'red')
+// {
+//     color = 'red';  
+// }
+
+// else if(mode === 'pink')
+//     {
+//         color = 'pink'; 
+// }
+
+// else{
+//     color = 'white';
+// }
+
+// console.log(color);
+
+// Ternary operator
+
+// let age = 8;
+
+// var res = age >= 18 ? "adult" : "not adult";
+
+// console.log(res)
+
+// let num = prompt("Enter a number");
+// if(num % 5 === 0)
+// {
+//     console.log("num is table of 5");
+// }
+
+// else 
+// {
+//     console.log("num is not table of 5");
+// }
